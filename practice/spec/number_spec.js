6f215e475fdf09545e8f9625dcc16ba00a9a5149
zhaomingyuan7 <zhaomingyuan7@gmail.com>
2014-05-21 16:31:52 +0800

    should the number length is four

--- practice/spec/number_spec.js
+++ practice/spec/number_spec.js
@@ -2,13 +2,14 @@ describe("Test_number", function () {
 
 
     beforeEach(function () {
-
+        init_number_database()
     });
+    afterEach(function(){
+        localStorage.clear();
+    })
 
     it("judge the number if is four digit", function () {
-        console.log('333',Test.is_test_number())
-// 
-
+//        console.log('333',Test.is_test_number())
 
     });
 

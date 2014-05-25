59cce26b1fdd8b2aec9617acf29dfba569e046a9
zhaomingyuan7 <zhaomingyuan7@gmail.com>
2014-05-25 11:51:05 +0800

    modify page information

---
+++ practice/spec/page_spec.js
@@ -0,0 +1,24 @@
+describe("page", function () {
+
+
+    beforeEach(function () {
+
+    });
+
+    afterEach(function(){
+
+    })
+
+    it("should can make start button going", function () {
+
+    });
+
+    it("should can make start button only can click six times", function () {
+
+    });
+
+    it("should show success when result is 4A0B", function () {
+
+    });
+
+});
--- practice/src/JudgeNumber.js
+++ practice/src/JudgeNumber.js
@@ -11,8 +11,6 @@ JudgeNumber.is_not_number = function(name){
     return isNaN(name) == 0
 }
 JudgeNumber.compare_number_with_input = function(number1,number2){
-    console.log(number1,'333')
-    console.log(number2,'222')
     var the_first = JudgeNumber.get_every_number(number1);
     var the_second = JudgeNumber.get_every_number(number2);
     var x = 0,y = 0;
@@ -20,14 +18,12 @@ JudgeNumber.compare_number_with_input = function(number1,number2){
        for(var j = 0;j < 4;j ++){
            if(the_first[i] == the_second[j] && i==j){
                 x ++;
-
            }
            if(the_first[i] == the_second[j] && i!=j){
                 y ++;
            }
        }
     }
-    console.log(x+'A'+y+'B','111')
     return x+'A'+y+'B';
 }
 JudgeNumber.get_every_number = function(number){
@@ -40,26 +36,26 @@ JudgeNumber.get_every_number = function(number){
     return every_number;
 }
 var z = 0;
-
-function print_number(btn){
+function print_number(){
+    var number = document.getElementById("name").value
     z++;
-    console.log(z,'z')
-    if(z < 7 && z > 5){
-        btn.disabled = true
-        document.getElementById("times").innerHTML = "你已竞猜六次"
+    if(z == 6){
+        document.getElementById('end').disabled = true
+        document.getElementById("times").innerHTML = "你已竞猜六次";
     }
-    var a = my.name.value;
-    console.log(Random.is_test_number(),'ddd')
-    var b = JudgeNumber.compare_number_with_input(a,Random.is_test_number())
-    document.getElementById("demo").innerHTML = b
-    console.log(z,'z')
-    localStorage.z = z;
-}
-function btnDisable(btn){
-   btn.disabled = true;
+    var b = JudgeNumber.compare_number_with_input(number,localStorage.current_number)
+    if(b == "4A0B"){
+        document.getElementById("demo").innerHTML = "成功猜对数字" + b;
+    }
+    else{
+        document.getElementById("demo").innerHTML = b;
+    }
 }
 function start_button(){
-    if(z == 6){
-     return true
-    }
+    Random.test_number;
+    document.getElementById('end').disabled = false;
+    document.getElementById('start').disabled = true;
+    document.getElementById('name').disabled = false
+    console.log(localStorage.current_number,'3333333333')
+
 }
